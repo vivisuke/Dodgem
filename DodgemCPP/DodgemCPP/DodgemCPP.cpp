a0ad613dfc99f8bf06f1f6e19c2f9e70646581b5
vivisuke <ntsuda@master.email.ne.jp>
2022-10-25 13:06:03 +0900

    0032 ◎ C 10/25 10/25 0.001│ToDo│どちらかがゴール or 移動不可になるまでランダムプレイ可能に

--- DodgemCPP/DodgemCPP/DodgemCPP.cpp
+++ DodgemCPP/DodgemCPP/DodgemCPP.cpp
@@ -1,15 +1,32 @@
 ﻿#include <iostream>
+#include <random>
 #include "Board.h"
 
 using namespace std;
+
+random_device g_rand;     	// 非決定的な乱数生成器
+mt19937 g_mt(g_rand());     // メルセンヌ・ツイスタの32ビット版
 
 int main()
 {
 	Board bd;
 	bd.print();
 	Moves lst;
-	bd.get_red_moves(lst);
-	bd.get_blue_moves(lst);
+	int ix;
+	for(;;) {
+		bd.get_red_moves(lst);
+		if( lst.empty() ) break;
+		ix = g_mt() % lst.size();
+		bd.do_move_red(lst[ix]);
+		bd.print();
+		if( bd.m_n_red == 0 ) break;
+		bd.get_blue_moves(lst);
+		if( lst.empty() ) break;
+		ix = g_mt() % lst.size();
+		bd.do_move_blue(lst[ix]);
+		bd.print();
+		if( bd.m_n_blue == 0 ) break;
+	}
 
     std::cout << "OK.\n";
 }
