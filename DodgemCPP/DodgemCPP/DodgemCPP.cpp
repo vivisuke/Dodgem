28c95f153c50c634b6d6d6e50aae71dffeded193
vivisuke <ntsuda@master.email.ne.jp>
2022-10-26 07:48:06 +0900

    0033 ◎ C 10/25 10/26 0.001│ToDo│深さ指定先読み関数

--- DodgemCPP/DodgemCPP/DodgemCPP.cpp
+++ DodgemCPP/DodgemCPP/DodgemCPP.cpp
@@ -7,7 +7,41 @@ using namespace std;
 random_device g_rand;     	// 非決定的な乱数生成器
 mt19937 g_mt(g_rand());     // メルセンヌ・ツイスタの32ビット版
 
+int g_count = 0;
 
+void search_game_tree(Board& bd, bool red_turn, int depth) {
+	if( depth == 0 ) {
+		cout << ++g_count << ":\n";
+		bd.print();
+	} else {
+		Moves lst;
+		if( red_turn ) {
+			bd.get_red_moves(lst);
+			if( lst.empty() ) {
+				cout << ++g_count << ":\n";
+				bd.print();
+				return;
+			}
+			for(int i = 0; i != lst.size(); ++i) {
+				bd.do_move_red(lst[i]);
+				search_game_tree(bd, !red_turn, depth-1);
+				bd.undo_move_red(lst[i]);
+			}
+		} else {
+			bd.get_blue_moves(lst);
+			if( lst.empty() ) {
+				cout << ++g_count << ":\n";
+				bd.print();
+				return;
+			}
+			for(int i = 0; i != lst.size(); ++i) {
+				bd.do_move_blue(lst[i]);
+				search_game_tree(bd, !red_turn, depth-1);
+				bd.undo_move_blue(lst[i]);
+			}
+		}
+	}
+}
 
 int main()
 {
@@ -16,6 +50,10 @@ int main()
 	Moves lst;
 	int ix;
 	if( true ) {
+		g_count = 0;
+		search_game_tree(bd, true, 3);
+	}
+	if( false ) {
 		bd.get_red_moves(lst);
 		ix = g_mt() % lst.size();
 		bd.do_move_red(lst[ix]);
